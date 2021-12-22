569b32e1db5affadfac1527db1a8c9d6b81e7320
Daniel Woodruff <92489496+ClarityCreated@users.noreply.github.com>
2021-12-22 14:49:27 -0800

    cart-view/cart-service
    Added cart-view component with html updates. Added methods for cart.service.ts.

--- revature-bookshelf-front/src/app/app.module.ts
+++ revature-bookshelf-front/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CartComponent } from './cart/cart.component';
+import { CartViewComponent } from './cart-view/cart-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CartComponent
+    CartViewComponent,
   ],
   imports: [
     BrowserModule,
--- revature-bookshelf-front/src/app/cart-view/cart-view.component.spec.ts
+++ revature-bookshelf-front/src/app/cart-view/cart-view.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CartComponent } from './cart.component';
+import { CartViewComponent } from './cart-view.component';
 
-describe('CartComponent', () => {
-  let component: CartComponent;
-  let fixture: ComponentFixture<CartComponent>;
+describe('CartViewComponent', () => {
+  let component: CartViewComponent;
+  let fixture: ComponentFixture<CartViewComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CartComponent ]
+      declarations: [ CartViewComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CartComponent);
+    fixture = TestBed.createComponent(CartViewComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
