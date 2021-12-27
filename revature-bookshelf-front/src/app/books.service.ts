ec70b6f3939158302d79bc6c0adc1d7ec2c45944
nlundgren <nilslundus@gmail.com>
2021-12-26 23:06:04 -0600

    FIX: changed route for all books req

--- revature-bookshelf-front/src/app/books.service.ts
+++ revature-bookshelf-front/src/app/books.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {BehaviorSubject, Subject} from "rxjs";
+import { BehaviorSubject, Subject } from "rxjs";
 
 import { HttpClient } from "@angular/common/http";
 
@@ -8,7 +8,7 @@ import { HttpClient } from "@angular/common/http";
 })
 export class BooksService {
 
-  private books:Array<any>=[
+  private books: Array<any> = [
     // {id:1,title:"War and Peace",pageCount:300,ISBN:"3645364",author:"Tolstoy",genre:"novel",imgPath:"../assets/images/crimeAndPunishment.jpg",quantity:"3"},
     // {id:2,title:"crime and Punishment",pageCount:300,ISBN:"3623794",author:"Dostoevsky",genre:"novel",imgPath:"",quantity:"7"},
     // {id:3,title:"white",pageCount:170,ISBN:"3647661",author:"Arthur",genre:"war",imgPath:"",quantity:"10"},
@@ -22,19 +22,19 @@ export class BooksService {
 
   constructor(private httpClient: HttpClient) { }
 
-  booksStream=new BehaviorSubject({
-    books:this.books
+  booksStream = new BehaviorSubject({
+    books: this.books
   });
 
 
   // getBooks(){
   //   return this.books;
   // }
-  
-  getBooks(){
-    this.httpClient.get("http://localhost:8080/book-list/all")
+
+  getBooks() {
+    this.httpClient.get("http://localhost:8080/books")
       .subscribe({
-        next: (response:any)=>{
+        next: (response: any) => {
           this.books = response;
           this.booksStream.next({
             books: this.books
